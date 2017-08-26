44139eda50d579f53856782cb385480e32cabe8d
Diego Tapias <diego@maths.usyd.edu.au>
2017-08-26 15:36:30 +1000

    Refactoring files

--- main.jl
+++ main.jl
@@ -1,15 +1,6 @@
 using DynamicalBilliards
 #DynamicalBilliards.enableplotting()
 
-bt = billiard_rectangle(r, r; setting = "periodic")
-d1 = Disk([0., 0.], a)
-d2 = Disk([r, 0.], a)
-d3 = Disk([0., r], a)
-d4 = Disk([r, r], a)
-dc = Disk([r/2, r/2], b)
-
-push!(bt, dc, d1,d2,d3,d4)  #Billiard_table
-
 function uniform_simulation(n::Int, table::Array{DynamicalBilliards.Obstacle,1}, t::AbstractFloat)
     res = zeros(n)
     for i in 1:n
